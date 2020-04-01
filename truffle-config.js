c6f86f643ae93a7980405aa47b030a8c326d404f
TSchellien <TSchellien@gmx.net>
2020-04-01 10:06:30 +0200

    added ropsten network

--- truffle-config.js
+++ truffle-config.js
@@ -5,12 +5,18 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
-  //networks: {
+  networks: {
   //  development: {
   //    host: "127.0.0.1",
   //    port: 7545,
   //    network_id: "*"
-  //  },
+    ropsten: {
+	    provider: function() {
+	      return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/YOUR-PROJECT-ID");
+	    },
+	    network_id: '3',
+    }
+  }
   //  test: {
   //    host: "127.0.0.1",
   //    port: 7545,
